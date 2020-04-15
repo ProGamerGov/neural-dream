f35bf1f11455da0aa84050ff140e602025f81df8
ProGamerGov <ProGamerGov@users.noreply.github.com>
2020-04-15 07:56:18 -0600

    Update __init__.py

--- neural_dream/neural_dream_core/models/__init__.py
+++ neural_dream/neural_dream_core/models/__init__.py
@@ -5,8 +5,8 @@ from neural_dream.neural_dream_core.models.googlenet.googlenetplaces import Goog
 from neural_dream.neural_dream_core.models.googlenet.googlenet_layer_names import googlenet_layer_names
 
 from neural_dream.neural_dream_core.models.inception.inception5h import Inception5h
+from neural_dream.models.inception.inceptionv3_keras import InceptionV3Keras
 from neural_dream.neural_dream_core.models.inception.inception_layer_names import inception_layer_names
 
 from neural_dream.neural_dream_core.models.resnet.resnet_50_1by2_nsfw import ResNet_50_1by2_nsfw
 from neural_dream.neural_dream_core.models.resnet.resnet_layer_names import resnet_layer_names
-
